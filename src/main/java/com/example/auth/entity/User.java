14903f0e10d2d74cc733f3e41f263b15e354c1a8
Moustansir-H <hassanalymoustansir@gmail.com>
2026-03-03 22:19:08 +0400

    Ajout de entite user et repository

---
+++ src/main/java/com/example/auth/entity/User.java
@@ -0,0 +1,30 @@
+package com.example.auth.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private LocalDateTime createdAt;
+
+    public User() {}
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+        this.createdAt = LocalDateTime.now();
+    }
+}
